5a3037b08da878b31e78d547bfb16b6cf9ca8252
Moshe Dayan <xynpyro@gmail.com>
2025-03-18 20:42:54 +0700

    created databasse seeder for category and jasa category

---
+++ database/seeders/JasaCategorySeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\JasaCategory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class JasaCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $jasa_categories = [
+            [
+                "name" => "Jasa Rumah Tangga",
+            ],
+            [
+                "name" => "Jasa Perbaikan & Instalasi",
+            ],
+            [
+                "name" => "Jasa Pendidikan & Bimbingan",
+            ],
+            [
+                "name" => "Jasa Kesehatan & Kecantikan",
+            ],
+            [
+                "name" => "Jasa Kreatif & Digital"
+            ],
+            [
+                "name" => "Jasa Event Organizer"
+            ],
+            [
+                "name" => "Jasa Penyewaan Barang",
+            ],
+        ];
+
+        JasaCategory::insert($jasa_categories);
+    }
+}
